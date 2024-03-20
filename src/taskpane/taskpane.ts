90296d3a5e5a724faee66a2ef25c96749bbb7678
Jason_buaa <waterworth@qq.com>
2024-03-20 13:52:14 +0800

    事件对象

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -30,10 +30,13 @@ function random(number) {
   return Math.floor(Math.random() * (number + 1));
 }
 
-btn.addEventListener("click", () => {
+function bgChange(e) {
   const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
-});
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
+}
+btn.addEventListener("click", bgChange);
+
 let savedConditionalFormats = [];
 async function setup() {
   await Excel.run(async (context) => {
